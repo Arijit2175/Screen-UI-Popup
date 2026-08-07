522fe3d96865497eabe6ef07e171f4474ccbb3de
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-08-07 21:15:34 +0530

    displaying the image

--- ui/overlay.py
+++ ui/overlay.py
@@ -1,16 +1,32 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QLabel
+from PySide6.QtGui import QPixmap
 from PySide6.QtCore import Qt
 
 class Overlay(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Spider Greeting")
-
-        self.setWindowFlag(Qt.FramelessWindowHint)
-
-        self.setWindowFlag(Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(
+            Qt.FramelessWindowHint |
+            Qt.WindowStaysOnTopHint
+        )
 
         self.setAttribute(Qt.WA_TranslucentBackground)
 
         self.resize(350, 450)
+        self.move(100, 100)
+
+        self.spiderman = QLabel(self)
+
+        pixmap = QPixmap("assets/spidey.jpg")
+
+        self.spiderman.setPixmap(
+            pixmap.scaled(
+                250,
+                250,
+                Qt.KeepAspectRatio,
+                Qt.SmoothTransformation
+            )
+        )
+
+        self.spiderman.move(50, 0)
