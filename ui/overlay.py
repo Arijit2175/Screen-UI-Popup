1db7ebb6730a51e83a422d29387f6e8714b56d55
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-08-07 21:11:09 +0530

    overlay window

---
+++ ui/overlay.py
@@ -0,0 +1,17 @@
+from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import Qt
+
+
+class Overlay(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Spider Greeting")
+
+        self.setWindowFlag(Qt.FramelessWindowHint)
+
+        self.setWindowFlag(Qt.WindowStaysOnTopHint)
+
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        self.resize(350, 450)
